c1911b23e99ae9e2106a110d6d06cd141e1d10c1
김지형 <jihyoung9912@gmail.com>
2023-05-10 00:29:47 +0900

    style: SignUp Form

--- src/constants/COLOR.ts
+++ src/constants/COLOR.ts
@@ -4,7 +4,7 @@ export const COLORS = {
   fontPrimary: '#1f1f1f',
   primary: '#a4508b',
   secondary: '#93487D',
-  primaryVariant: '#98B2C9',
+  primaryVariant: '#e0dede',
   secondaryVariant: '#FFB49A',
   Tertiary: '#E9DECF',
   gray: '#888888',
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { useSignup } from '../hooks/useSignUp';
+import { useSignup } from 'hooks/useSignUp';
+import styled from 'styled-components';
+import { COLORS } from 'constants/COLOR';
 
 const SignUp = () => {
   const [email, setEmail] = useState('');
@@ -28,10 +30,64 @@ const SignUp = () => {
     }
   };
 
+  const Container = styled.div`
+    width: 350px;
+    height: 400px;
+    overflow: hidden;
+    border-radius: 10px;
+    box-shadow: 5px 5px 20px #000;
+    margin-top: 50px;
+  `;
+
+  const SignLabel = styled.label`
+    display: flex;
+    color: ${COLORS.fontPrimary};
+    font-size: 2.3em;
+    justify-content: center;
+    margin: 60px 60px 40px 60px;
+    font-weight: bold;
+    cursor: pointer;
+  `;
+
+  const Input = styled.input`
+    display: flex;
+    justify-content: center;
+    width: 60%;
+    height: 30px;
+    background: ${COLORS.primaryVariant};
+    margin: 20px auto;
+    padding: 10px;
+    border: none;
+    outline: none;
+    border-radius: 5px;
+  `;
+
+  const SubmitButton = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 60%;
+    height: 40px;
+    margin: 20px auto;
+    color: ${COLORS.white};
+    background: ${COLORS.primary};
+    font-size: 1em;
+    font-weight: bold;
+    outline: none;
+    border: none;
+    border-radius: 5px;
+    transition: 0.2s ease-in;
+    cursor: pointer;
+    :hover {
+      background: ${COLORS.secondary};
+    }
+  `;
+
   return (
-    <div>
+    <Container>
       <form>
-        <input
+        <SignLabel>Wine-Trail</SignLabel>
+        <Input
           name="email"
           type="text"
           placeholder="Email"
@@ -39,15 +95,7 @@ const SignUp = () => {
           onChange={onChange}
           required
         />
-        <input
-          name="displayName"
-          type="text"
-          placeholder="NickName"
-          value={displayName}
-          onChange={onChange}
-          required
-        />
-        <input
+        <Input
           name="password"
           type="password"
           placeholder="Password"
@@ -55,9 +103,19 @@ const SignUp = () => {
           onChange={onChange}
           required
         />
-        <input type="submit" onClick={SignUp} value="Sign Up" />
+        <Input
+          name="displayName"
+          type="text"
+          placeholder="NickName"
+          value={displayName}
+          onChange={onChange}
+          required
+        />
+        <SubmitButton type="submit" onClick={SignUp}>
+          Sign Up
+        </SubmitButton>
       </form>
-    </div>
+    </Container>
   );
 };
 
