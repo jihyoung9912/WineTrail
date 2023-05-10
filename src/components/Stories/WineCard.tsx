27df10684e644eb34597c86ab9f725bd979b6506
김지형 <jihyoung9912@gmail.com>
2023-05-10 22:39:44 +0900

    feat: Wine Stories

--- src/components/Stories/WineCard.tsx
+++ src/components/Stories/WineCard.tsx
@@ -2,6 +2,7 @@ import { COLORS } from 'constants/COLOR';
 import styled from 'styled-components';
 // eslint-disable-next-line import/named
 import { DocumentData } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react';
 
 const Card = styled.div`
   display: flex;
@@ -30,35 +31,60 @@ const WineInfoSection = styled.div`
 const WineName = styled.div`
   width: 100%;
   font-size: 1.5rem;
+  font-weight: bold;
 `;
 
 const WineVintage = styled.div`
   width: 100%;
-  font-size: 1.5rem;
-`;
-
-const WinePrice = styled.div`
-  width: 100%;
-  font-size: 1.5rem;
+  font-size: 1.1rem;
 `;
 
 const WineSatisfaction = styled.div`
   width: 100%;
-  font-size: 1.5rem;
+  font-size: 1.1rem;
+`;
+
+const CreatedAt = styled.div`
+  width: 100%;
+  font-size: 1.1rem;
+  color: gray;
 `;
 
 const WineCard = (wineData: DocumentData) => {
+  const [satisfaction, setSatisfaction] = useState('');
   const data = wineData.wineData.data;
   console.log(data);
+
+  useEffect(() => {
+    setSatisfaction(data?.data?.wineData?.satisfaction);
+  }, [data]);
+
+  switch (satisfaction) {
+    case '0':
+      setSatisfaction('😍 최고예요');
+      break;
+    case '1':
+      setSatisfaction('😊️ 만족했어요');
+      break;
+    case '2':
+      setSatisfaction('🙂 괜찮았어요');
+      break;
+    case '3':
+      setSatisfaction('😕 그저 그래요');
+      break;
+    case '4':
+      setSatisfaction('😔 별로예요');
+      break;
+  }
 
   return (
     <Card>
       <WineImg src={`${process.env.PUBLIC_URL}/assets/images/WineImg.png`} alt="WineImg" />
       <WineInfoSection>
-        <WineName>와인 이름</WineName>
-        <WineVintage>와인 이름</WineVintage>
-        <WinePrice>와인 이름</WinePrice>
-        <WineSatisfaction>와인 이름</WineSatisfaction>
+        <WineName>{data?.data?.wineData?.wineName}</WineName>
+        <WineVintage>{data?.data?.wineData?.vintage}년</WineVintage>
+        <WineSatisfaction>{satisfaction}</WineSatisfaction>
+        <CreatedAt>{data.createAt}</CreatedAt>
       </WineInfoSection>
     </Card>
   );
